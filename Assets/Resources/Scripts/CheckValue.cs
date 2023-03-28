ba0602cceae3b0ad46c310b917497f7a4d04dcb5
VictorCyprien <victor.cyprien@limayrac.fr>
2023-03-28 13:45:12 +0200

    Update description level when changing level difficulty

--- Assets/Resources/Scripts/CheckValue.cs
+++ Assets/Resources/Scripts/CheckValue.cs
@@ -9,6 +9,48 @@ public class CheckValue : MonoBehaviour
 {
     public Scrollbar difficulty_selector;
     public TextMeshProUGUI difficulty_text;
+    public TextMeshProUGUI description_text;
+
+    string build_new_description(int level_difficulty){
+        string msg = "";
+        
+        switch (level_difficulty)
+        {
+            case 1:
+                msg += "- Environnement calme\n- Un son ou une image qui peut faire pense à la phobie en question";
+                break;
+
+            case 2:
+                msg += "- Environnement calme\n- Un son de la phobie qui fait peur va être entendu";
+                break;
+
+            case 3:
+                msg += "- Environnement moins tranquille\n- Une image lointaine qui invoque la phobie apparaît";
+                break;
+
+            case 4:
+                msg += "- Environnement calme\n- Une image d'un jouet/élément qui ne fait pas peur mais concerne la phobie apparaît";
+                break;
+
+            case 5:
+                msg += "- Environnement moins tranquille\n- Image de la phobie mais présence d'éléments rassurants";
+                break;
+
+            case 6:
+                msg += "- Environnement moins tranquille\n- Une image en 3D de la phobie apparaît";
+                break;
+
+            case 7:
+                msg += "- Environnement moins tranquille\n- Une image en 3D de la phobie apparaît avec le son";
+                break;
+
+            case 8:
+                msg += "- Environnement hostile\n- Une image en 3D de la phobie apparaît ainsi que le son";
+                break;
+        }
+
+        return msg;
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,10 +60,10 @@ public class CheckValue : MonoBehaviour
             // round to int to catch floating point problems.
             int currentStep = Mathf.RoundToInt(value / (1f / (float) difficulty_selector.numberOfSteps)) + 1;
             if (value > 0.5){
-                currentStep -= 1;
+                currentStep -= 3;
             }
             difficulty_text.text = "Niveau " + currentStep.ToString();
-            Debug.Log($"Value of Selector : {difficulty_selector.value}");
+            description_text.text = build_new_description(currentStep);
             Debug.Log($"Value of Difficulty : {difficulty_text.text}");
         });
     }
