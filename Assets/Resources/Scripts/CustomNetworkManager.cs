597a93a81f4d14a3d333e55ea45f95f9cca5acd8
VictorCyprien <victor.cyprien@limayrac.fr>
2023-07-13 10:31:59 +0200

    Add overrided method `OnServerDisconnect` and `OnClientDisconnect`

--- Assets/Resources/Scripts/CustomNetworkManager.cs
+++ Assets/Resources/Scripts/CustomNetworkManager.cs
@@ -9,8 +9,12 @@ public class CustomNetworkManager : NetworkManager
 {
     public GameObject waitingScreen;
     public GameObject vueServeur;
+    public GameObject checkPlatform;
+    public GameObject waitingCamera;
 
     public override void OnServerConnect(NetworkConnectionToClient conn){
+        Debug.Log("Client connected !");
+
         // We get 'Waiting' canva and hide his object (child + parent)
         var waitingClient = waitingScreen.transform.GetChild(0).gameObject;
         waitingClient.SetActive(false);
@@ -18,6 +22,33 @@ public class CustomNetworkManager : NetworkManager
 
         // Show VueServeur (Only for server !)
         vueServeur.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public override void OnServerDisconnect(NetworkConnectionToClient conn)
+    {
+        Debug.Log("Client disconnected !");
+        NetworkServer.DestroyPlayerForConnection(conn);
+
+        var waitingClient = waitingScreen.transform.GetChild(0).gameObject;
+        waitingClient.SetActive(true);
+        waitingScreen.SetActive(true);
+
+        // Hide VueServeur (Only for server !)
+        vueServeur.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    public override void OnClientDisconnect(){
+        // Get and Show the research for server canva
+        var waitingForServer = waitingScreen.transform.GetChild(1).gameObject;
+
+        waitingForServer.SetActive(true);
+        waitingScreen.SetActive(true);
+
+        // Create a new temporary camera + Restart the discovery
+        // This is used to avoid to restart the application in Android
+        Debug.Log("Connection Lost !\n Research new server...");
+        waitingCamera.GetComponent<CreateWaitingForServerCamera>().CreateCamera();
+        checkPlatform.GetComponent<CheckPlatform>().SetupClient();
     }
 
 }
